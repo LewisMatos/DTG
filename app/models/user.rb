11aa295ce21de0ff6c3237edca9d089dab177ade
github username <mendelblesofsky@gmail.com>
2015-10-26 16:28:36 -0400

    fix logic in model, put link to pin event in show

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,37 +22,37 @@ class User < ActiveRecord::Base
 	# this is a sequel query which will eliminate the users who have liked you, and you have also liked.
   	# it will then filter out the same gender as you (should be change to reflect your sexual preference)
   	# and will only find the users who have pinned this event.
-	def self.find_match(event, current_user)
+	def find_match(event)
 		sequel = %Q(
 	    	SELECT * FROM users WHERE users.id IN 
 	    	(SELECT also_likes_me.user_id FROM user_events INNER JOIN user_events AS also_likes_me
 	      	ON user_events.user_id = also_likes_me.shown_user_id 
 	      	AND also_likes_me.liked = 'yes' AND also_likes_me.event_id = #{event.id} AND user_events.event_id = #{event.id}
-	      	WHERE user_events.user_id = #{current_user.id} AND user_events.liked = 'yes' AND user_events.event_id = #{event.id}) AND users.gender != "#{current_user.gender}" AND users.id IN (SELECT user_events.user_id FROM user_events 
+	      	WHERE user_events.user_id = #{self.id} AND user_events.liked = 'yes' AND user_events.event_id = #{event.id}) AND users.gender != "#{self.gender}" AND users.id IN (SELECT user_events.user_id FROM user_events 
 	     	WHERE user_events.event_id = #{event.id})
     	)
 		User.find_by_sql(sequel)
 	end
 
 	# this query will filter out any user you have liked or disliked, and is opposite gender and pinned this event.
-	def self.eliminate_swiped_users(event, current_user)
+	def eliminate_swiped_users(event)
 		sequel = %Q(
     		SELECT * FROM users WHERE users.id NOT IN
     		(SELECT user_events.shown_user_id FROM user_events 
-      		WHERE user_events.user_id = #{current_user.id} AND liked != 'nil' AND user_events.event_id = #{event.id})
-      		AND users.id IN (SELECT users.id FROM users WHERE users.gender != "#{current_user.gender}") AND users.id IN (SELECT user_events.user_id FROM user_events 
+      		WHERE user_events.user_id = #{self.id} AND liked != 'nil' AND user_events.event_id = #{event.id})
+      		AND users.id IN (SELECT users.id FROM users WHERE users.gender != "#{self.gender}") AND users.id IN (SELECT user_events.user_id FROM user_events 
      		WHERE user_events.event_id = #{event.id})
    		)
 		User.find_by_sql(sequel)
 	end
 
-	def self.get_user(event, current_user)
-		@match = find_match(event, current_user)
-		@swiped = eliminate_swiped_users(event, current_user)
+	def get_user(event)
+		@match = find_match(event)
+		@swiped = eliminate_swiped_users(event)
 		if @match.length > 0
-			@match.first
+			[@match.first, true]
 		elsif  @swiped.length > 0
-			@swiped.first
+			[@swiped.first, false]
 		else
 			nil
 		end
