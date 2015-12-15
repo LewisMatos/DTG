585eda631928c9e3068a5b70a238ca1bf5266662
Stephen Koltz <steve.koltz@gmail.com>
2015-12-14 23:04:44 -0500

    recommit for initializer devise rb file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,9 +238,9 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   # local: 
-config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92",:callback_url => 'http://localhost:3000/users/auth/facebook/callback/',info_fields: 'email,name,gender,bio,about,interested_in,cover',scope: 'user_birthday,user_photos',:display => "popup"
+# config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92",:callback_url => 'http://localhost:3000/users/auth/facebook/callback/',info_fields: 'email,name,gender,bio,about,interested_in,cover',scope: 'user_birthday,user_photos',:display => "popup"
   # for heroku:
-	# config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92",:callback_url => 'https://downtogo.herokuapp.com/users/auth/facebook/callback/',info_fields: 'email,name,gender,bio,about,interested_in,cover',scope: 'user_birthday,user_photos',:display => "popup"
+	config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92",:callback_url => 'https://downtogo.herokuapp.com/users/auth/facebook/callback/',info_fields: 'email,name,gender,bio,about,interested_in,cover',scope: 'user_birthday,user_photos',:display => "popup"
 
 
 	# ==> Warden configuration
